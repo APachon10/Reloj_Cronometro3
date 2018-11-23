9540a5d969c9c8f1cebe0673e419f33dd6450d59
APachon10 <Pachondestroy95@gmail.com>
2018-11-23 16:19:26 +0100

    Implementacion de Reloj y preparado para exportar como jar

--- src/Pruebas/RelojDigital_Prueba.java
+++ src/Pruebas/RelojDigital_Prueba.java
@@ -2,18 +2,31 @@ package Pruebas;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class RelojDigital_Prueba extends JFrame {
-
+public class RelojDigital_Prueba extends JTextField implements ActionListener {
+	private JFrame ventanaReloj;
+	private JLabel label_reloj;
+	
 	public RelojDigital_Prueba() {
+		ventanaReloj = new JFrame("Reloj Digital ");
 		
+		ventanaReloj.setVisible(true);
+		ventanaReloj.setSize(400, 200);
 	}
 	public static void main(String[] args) {
 		new RelojDigital_Prueba();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 
 }
