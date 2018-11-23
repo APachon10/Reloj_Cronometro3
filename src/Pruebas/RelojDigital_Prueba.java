144817dfc74cc537cd9046c03f59ad4ee9ed33cd
APachon10 <Pachondestroy95@gmail.com>
2018-11-23 14:22:28 +0100

    Initial Commit

---
+++ src/Pruebas/RelojDigital_Prueba.java
@@ -0,0 +1,19 @@
+package Pruebas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class RelojDigital_Prueba extends JFrame {
+
+	public RelojDigital_Prueba() {
+		
+	}
+	public static void main(String[] args) {
+		new RelojDigital_Prueba();
+	}
+
+}
